1ec4d838a435bfead961a54b78569db331e8ddf0
Bao Tran Ha <hbtran2004tnct@gmail.com>
2025-01-08 23:59:21 +0700

    first

---
+++ odaurehonbe/Migrations/20250104140659_202501042109.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace odaurehonbe.Migrations
+{
+    /// <inheritdoc />
+    public partial class _202501042109 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerID",
+                table: "Payments",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "CustomerID",
+                table: "Payments",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+        }
+    }
+}
